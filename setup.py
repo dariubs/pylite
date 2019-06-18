79258a07b8d84e7df802fc57721bb339b2f589c5
Dariush Abbasi <dariushem@yahoo.com>
2019-06-18 11:53:44 +0430

    update setup file

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
-#!/usr/bin/python3
+import setuptools
 
-from distutils.core import setup
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-setup(name='pylite',
-      version='0.1.0',
-      description='Intract with sqlite3 in python as simple as it can be.',
-      author='Dariush Abbasi',
-      author_email='poshtehani@gmail.com',
-      url='http://dariubs.github.io/pylite',
-      packages=['pylite'],
-      package_dir={'pylite': 'pylite'},
-      license='MIT'
-     )
+setuptools.setup(
+    name='pylite',
+    version='0.1.0',
+    description='Intract with sqlite3 in python as simple as it can be.',
+    author='Dariush Abbasi',
+    author_email='poshtehani@gmail.com',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='http://dariubs.github.io/pylite',
+    packages=setuptools.find_packages(),
+    package_dir={'pylite': 'pylite'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    license='MIT'
+)
